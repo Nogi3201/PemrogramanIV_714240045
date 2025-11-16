97e49d2daa668a49a0e29b48bd9e12827e74d8ac
Hasan Zubeir Pohan <hasanpohan035@gmail.com>
2025-11-16 21:08:35 +0700

    SAVE

---
+++ P5/Praktikum/Memakai kontrol TextBox, Checkbox, RadioButton, StatusBar, dan MessageBox/P5_3_714240045/P5_3_714240045/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P5_3_714240045
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void rb_iOS_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnKeluar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTampilkan_Click(object sender, EventArgs e)
+        {
+            string os = "";
+            string status = "";
+
+            if (rb_android.Checked == true)
+            {
+                os = "Android";
+            }
+            else if (rb_iOS.Checked == true)
+            {
+                os = "iOS";
+            }
+            if (cbYa.Checked == true )
+            {
+                status = "Ya, sudah diperbaiki";
+            }
+            else if (cbYa.Checked == false)
+            {
+                status = "Belum Diperbaiki";
+            }
+
+                MessageBox.Show(
+                    "Merk HP: " + txtMerkHP.Text +
+                    "\nSistem Operasi : " + os +
+                    "\nStatus Perbaikan : " + status,
+                    "Informasi Service HP",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information); 
+        }
+    }
+}
---
+++ P5/Praktikum/Membuat aplikasi MDI dan mengatur perletakan kendali/P5_2_714240045/P5_2_714240045/ParentForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P5_2_714240045
+{
+    public partial class ParentForm : Form
+    {
+        public ParentForm()
+        {
+            InitializeComponent();
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+                ChildForm childForm = new ChildForm();
+                childForm.MdiParent = this;
+                childForm.Show();
+        }
+
+        private void ExitMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void WindowCascadeMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.Cascade);
+        }
+
+        private void WindowTileMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.TileHorizontal);
+        }
+    }
+}
