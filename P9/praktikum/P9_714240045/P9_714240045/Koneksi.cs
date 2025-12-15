93d40b0320338647a956608d336bb6c97e1a7863
Hasan Zubeir Pohan <hasanpohan035@gmail.com>
2025-12-15 12:10:59 +0700

    save

---
+++ P9/praktikum/P9_714240045/P9_714240045/Koneksi.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace P9_714240045
+{
+    internal class Koneksi
+    {
+        string connectionstring = "Server=localhost;Database=pemrog2ulbi;Uid=root;Pwd='';";
+        MySqlConnection kon;
+
+        public void OpenConnection()
+        {
+            kon = new MySqlConnection(connectionstring);
+            kon.Open();
+        }
+
+        public void CloseConnection()
+        {
+            kon.Close();
+        }
+
+        public object ShowData(string query)
+        {
+            OpenConnection();
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter(query, kon);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+
+            CloseConnection();
+            return table;
+        }
+    }
+}
