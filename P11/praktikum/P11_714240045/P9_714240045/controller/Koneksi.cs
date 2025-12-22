fc974a5efe9903091c13cfdf1ff35b45916dde4f
Hasan Zubeir Pohan <hasanpohan035@gmail.com>
2025-12-22 15:51:16 +0700

    save

---
+++ P11/praktikum/P11_714240045/P9_714240045/controller/Koneksi.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace P9_714240045
+{
+    internal class Koneksi
+    {
+        string connectionstring = "Server=localhost;Database=pemrog2ulbi;Uid=root;Pwd='';";
+        MySqlConnection kon;
+
+        public void OpenConnection()
+        {
+            kon = new MySqlConnection(connectionstring);
+            kon.Open();
+        }
+
+        public void CloseConnection()
+        {
+            kon.Close();
+        }
+
+        public object ShowData(string query)
+        {
+            OpenConnection();
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter(query, kon);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+
+            CloseConnection();
+            return table;
+        }
+
+        public void ExecuteQuery(MySqlCommand command)
+        {
+            command.Connection = kon;
+            command.ExecuteNonQuery();  
+        }
+
+        public object ShowDataParam(string query, params MySqlParameter[] parameters)
+        {
+            OpenConnection();
+
+            MySqlCommand cmd = new MySqlCommand(query, kon);
+            cmd.Parameters.AddRange(parameters);
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+
+            CloseConnection();
+            return table;
+        }
+
+        public MySqlDataReader reader(string query)
+        {
+            MySqlCommand cmd = new MySqlCommand(query, kon);
+            MySqlDataReader dr = cmd.ExecuteReader();
+            return dr;
+        }
+
+    }
+}
---
+++ P11/praktikum/P11_714240045/P9_714240045/view/ParentForm.cs
@@ -0,0 +1,46 @@
+﻿using P9_714240045.view;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P9_714240045
+{
+    public partial class ParentForm : Form
+    {
+        public ParentForm()
+        {
+            InitializeComponent();
+        }
+
+        private void dataMahasiswaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form1 formMhs = new Form1();
+            formMhs.MdiParent = this;
+            formMhs.Show();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void ParentForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataNilaiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormNilai formNilai = new FormNilai();
+            formNilai.MdiParent = this;
+            formNilai.Show();
+        }
+
+    }
+}
