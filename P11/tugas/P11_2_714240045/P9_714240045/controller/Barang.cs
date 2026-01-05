7c16e5a340980f75b2853f3eb0b43b12d92e649e
Hasan Zubeir Pohan <hasanpohan035@gmail.com>
2026-01-05 14:58:57 +0700

    save

---
+++ P11/tugas/P11_2_714240045/P9_714240045/controller/Barang.cs
@@ -0,0 +1,62 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P9_714240045.model
+{
+    class Barang
+    {
+        Koneksi koneksi = new Koneksi();
+
+        // INSERT
+        public void Insert(M_barang m)
+        {
+            string query = "INSERT INTO t_barang (nama_barang, harga) VALUES (@nama, @harga)";
+            MySqlCommand cmd = new MySqlCommand(query, koneksi.GetConn());
+            cmd.Parameters.AddWithValue("@nama", m.NamaBarang);
+            cmd.Parameters.AddWithValue("@harga", m.Harga);
+            cmd.ExecuteNonQuery();
+        }
+
+        // UPDATE
+        public void Update(M_barang m, string id)
+        {
+            string query = "UPDATE t_barang SET nama_barang=@nama, harga=@harga WHERE id_barang=@id";
+            MySqlCommand cmd = new MySqlCommand(query, koneksi.GetConn());
+            cmd.Parameters.AddWithValue("@nama", m.NamaBarang);
+            cmd.Parameters.AddWithValue("@harga", m.Harga);
+            cmd.Parameters.AddWithValue("@id", id);
+            cmd.ExecuteNonQuery();
+        }
+
+        // DELETE
+        public void Delete(string id)
+        {
+            string query = "DELETE FROM t_barang WHERE id_barang=@id";
+            MySqlCommand cmd = new MySqlCommand(query, koneksi.GetConn());
+            cmd.Parameters.AddWithValue("@id", id);
+            cmd.ExecuteNonQuery();
+        }
+
+        public DataTable Show()
+        {
+            string query = "SELECT * FROM t_barang";
+            return (DataTable)koneksi.ShowData(query);
+        }
+
+        public DataTable Search(string keyword)
+        {
+            string query = "SELECT * FROM t_barang WHERE nama_barang LIKE @param";
+            return (DataTable)koneksi.ShowDataParam(
+                query,
+                new MySqlParameter("@param", "%" + keyword + "%")
+            );
+        }
+
+    }
+}
+
---
+++ P11/tugas/P11_2_714240045/P9_714240045/view/ParentForm.cs
@@ -0,0 +1,59 @@
+﻿using P9_714240045.view;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P9_714240045
+{
+    public partial class ParentForm : Form
+    {
+        public ParentForm()
+        {
+            InitializeComponent();
+        }
+
+        private void dataMahasiswaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form1 formMhs = new Form1();
+            formMhs.MdiParent = this;
+            formMhs.Show();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void ParentForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataNilaiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormNilai formNilai = new FormNilai();
+            formNilai.MdiParent = this;
+            formNilai.Show();
+        }
+
+        private void dataMasterBarangToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            FormBarang fb = new FormBarang();
+            fb.MdiParent = this;
+            fb.Show();
+        }
+
+        private void dataTransaksiToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            FormTransaksi ft = new FormTransaksi();
+            ft.MdiParent = this;
+            ft.Show();
+        }
+    }
+}
